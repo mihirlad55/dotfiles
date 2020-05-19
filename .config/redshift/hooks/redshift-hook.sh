47141c76cbaf691bb5e187ab11163e3e95271a20
Mihir Lad <mihirlad55@gmail.com>
2020-05-18 21:20:15 -0400

    Add icon to redshift notifications

--- .config/redshift/hooks/redshift-hook.sh
+++ .config/redshift/hooks/redshift-hook.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/sh
 
+BASE_DIR="$( readlink -f "$(dirname "$0")" )"
+ICON_PATH="$BASE_DIR/nightmode.png"
+
 case $1 in
     period-changed)
-        notify-send "Redshift" "Period changed to $3"
+        notify-send "Redshift" "Period changed to $3" -i "$ICON_PATH" -u normal
 esac
