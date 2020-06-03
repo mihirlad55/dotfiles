c04d696f3a4f0f902ffc45409f38da5d9f28efe7
Mihir Lad <mihirlad55@gmail.com>
2020-06-02 21:55:08 -0400

    Add redshift module to polybar
    This module shows an icon representing the redshift period.

--- .config/redshift/hooks/redshift-hook.sh
+++ .config/redshift/hooks/redshift-hook.sh
@@ -6,4 +6,6 @@ ICON_PATH="$BASE_DIR/nightmode.png"
 case $1 in
     period-changed)
         notify-send "Redshift" "Period changed to $3" -i "$ICON_PATH" -u normal
+        echo "$3" > /tmp/.redshift-period
+        ;;
 esac
